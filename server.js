e5e850e056b4c50f43484a1a0cc09490e2601434
Dennis <dennisweller@gmx.net>
2020-05-08 14:26:35 +0200

    Add basic route to restaurants

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const client = require('./Database/database.js')
+const client = require('./Database/database')
 const app = express();
+const port = 5000;
+
+const restaurants = require('./Routes/restaurant');
+const tags = require('./Routes/tag');
+const cities = require('./Routes/city');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const port = 5000;
+app.use('/restaurants', restaurants);
+//app.use('/tags', tags);
+//app.use('/cities', cities);
 
 app.get('/', (req, res) => res.json({success: 'true'}));
+
 
 app.listen(
     port, () => console.log(`Server listening on port ${port}`)

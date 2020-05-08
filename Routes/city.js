8ca4d04f6da2ba7384cb3b0fc9700e69bf472529
Mad Ting <samtastic1337@gmail.com>
2020-05-08 16:11:51 +0200

    Working?

--- Routes/city.js
+++ Routes/city.js
@@ -3,9 +3,32 @@ const router = express.Router();
 const client = require("../Database/database")
 
 router.get('/', (req, res) => {
-    res.send('route city');
+ client
+ .query("SELECT * FROM city")
+ .then ((data) => res.json(data))
+ .catch ((err) => res.send(err))
 });
 
+router.get('/:id', (req, res) => {
+const { id }  =   req.params.id
+
+client
+.query("SELECT * FROM city WHERE id=$1;",[id])
+.then ((data) => res.json(data))
+.catch ((err) => res.send(err))
+
+});
+
+
+router.post('/', (req, res) =>{
+
+const { name } = req.body 
+
+client
+.query("INSERT INTO city(name) VALUES($1);",[name])
+.then((data) => res.json(data))
+.catch((err) => res.json(err))
+})
 
 
 
--- Routes/tag.js
+++ Routes/tag.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const router = express.Router();
+<<<<<<< HEAD
 const client = require("../Database/database")
+=======
+const client = require('../Database/database');
+
+
+>>>>>>> 2f7c169714f6c6f1a0cf48939128040d61be0f0b
 
 router.get('/', (req, res) => {
-    res.send('route tag');
+    client
+    .query('SELECT * FROM tag;')
+    .then(data => res.json(data))
+    .catch(e => console.log(e));
 });
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    client
+    .query('SELECT * FROM "tag" WHERE id=$1;', [id])
+    .then(data => res.json(data))
+    .catch(e => console.log(e));
+})
+
+router.post('/', (req, res) => {
+    const {name} = req.body;
+    client
+    .query('INSERT INTO tag(name) values($1);', [name])
+    .then(data => res.json(data))
+    .catch(e => console.log(e));
+})
 
 module.exports = router;
 

991534033cba03df4954dd5ef42695d99af271d1
Mad Ting <samtastic1337@gmail.com>
2020-05-08 11:46:36 +0200

    Aded one line

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const client = require('./Database/database.js')
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
